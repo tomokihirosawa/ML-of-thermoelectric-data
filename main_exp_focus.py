8219dcc5c738f3cf99302b2b05f50341764cb61f
tomokihirosawa <tomoki.hirosawa@hotmail.co.jp>
2022-09-20 10:46:26 +0900

    Add files via upload

--- main_exp_focus.py
+++ main_exp_focus.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 
 # load PyTorch modules
 import torch
-from torch.utils import data # to define a dataset class
+from torch.utils import data  # to define a dataset class
 import torch.nn as nn
 #import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim import lr_scheduler
 
-# set random seed for reproducibility 
+# set random seed for reproducibility
 seed = 200
 np.random.seed(seed)
 torch.manual_seed(seed)
@@ -25,88 +25,101 @@ if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def train(device, epoch, params, model, optimizer,Tot_epoch,loss_array,x,y,alpha,cond,Delta):
+
+def train(device, epoch, params, model, optimizer, Tot_epoch, loss_array, x, y,
+          alpha, cond, Delta):
     # define data set and associated loader
-    loaded_set = dataloader.Dataset(x,y)
+    loaded_set = dataloader.Dataset(x, y)
     loader = data.DataLoader(loaded_set, **params)
 
     # set model to training mode
     model.train()
-    Wc=10.0
-    sample=1
-    size=41
+    Wc = 10.0
+    sample = 1
+    size = 41
     epoch_loss = 0.0
     # dict that consists of two lists to store inputs and labels
-    results = {'inputs':[], 'preds':[], 'labels':[]}
+    results = {'inputs': [], 'preds': [], 'labels': []}
 
     for inputs, labels in loader:
-        inputs = inputs[:,0].to(device)
-        labels = labels[:,0].to(device)
-        
+        inputs = inputs[:, 0].to(device)
+        labels = labels[:, 0].to(device)
+
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # computed values of L11 and L12 from the NN
-        preds = model(inputs[:size],Wc,alpha,cond,Delta)
+        preds = model(inputs[:size], Wc, alpha, cond, Delta)
         #the penalty term of negative spectral conductivity
-        b=1.0
-        preds[-1]=preds[-1]*b
-        
+        b = 1.0
+        preds[-1] = preds[-1] * b
+
         #evaluate loss
-        loss=criterion(preds, labels)   
-        
+        loss = criterion(preds, labels)
+
         # backpropagation and parameter updates
         loss.backward()
         optimizer.step()
 
         # running loss update
-        epoch_loss += loss*inputs.size(0)
+        epoch_loss += loss * inputs.size(0)
 
         with torch.no_grad():
             # push data to dict
             results['inputs'].append(inputs.detach().cpu().numpy())
             results['preds'].append(preds.detach().cpu().numpy())
             results['labels'].append(labels.detach().cpu().numpy())
-            
-    epoch_loss = epoch_loss/len(loaded_set)
-    loss_array[epoch]=epoch_loss.detach()
-    
-    #generate output files after all epochs 
-    if (epoch+1==hyperparameters.epochs):
+
+    epoch_loss = epoch_loss / len(loaded_set)
+    loss_array[epoch] = epoch_loss.detach()
+
+    #generate output files after all epochs
+    if (epoch + 1 == hyperparameters.epochs):
         print('epoch {} Training Loss: {:.4f}'.format(epoch, epoch_loss))
-        
+
         #output of NN
-        O=model.fc_layers(inputs[:size]).detach().cpu().numpy()
-        
+        O = model.fc_layers(inputs[:size]).detach().cpu().numpy()
+
         ##output the spectral conductivity
         # averaging 10 consequtive elements of the output vector to compute the spectral conductivity
-        Q=O[:size*4+10]
-        Sigma=np.zeros(size*4+1)  
+        Q = O[:size * 4 + 10]
+        Sigma = np.zeros(size * 4 + 1)
         for j in range(Sigma.shape[0]):
-            Sigma[j]=(Q[j]+Q[j+1]+Q[j+2]+Q[j+3]+Q[j+4]+Q[j+5]+Q[j+6]+Q[j+7]+Q[j+8]+Q[j+9])/10.0
-        res=np.zeros((2,size*4+1))
-        res[0]=np.linspace(-1.0, 1.0, size*4+1)*Wc*2
-        res[1]=Sigma+cond.detach().cpu().numpy()
-        
+            Sigma[j] = (Q[j] + Q[j + 1] + Q[j + 2] + Q[j + 3] + Q[j + 4] +
+                        Q[j + 5] + Q[j + 6] + Q[j + 7] + Q[j + 8] +
+                        Q[j + 9]) / 10.0
+        res = np.zeros((2, size * 4 + 1))
+        res[0] = np.linspace(-1.0, 1.0, size * 4 + 1) * Wc * 2
+        res[1] = Sigma + cond.detach().cpu().numpy()
+
         ##output the chemical potential
-        chemQ=O[size*4+10:]
-        chem=alpha.detach().cpu().numpy()+np.arctan(chemQ)/(np.pi/2.0)*Wc*Delta     
-        
+        chemQ = O[size * 4 + 10:]
+        chem = alpha.detach().cpu().numpy() + np.arctan(chemQ) / (
+            np.pi / 2.0) * Wc * Delta
+
         ##output L11 and L12 computed from the spectral conductivity
-        res2=np.zeros((sample,6,size))
+        res2 = np.zeros((sample, 6, size))
         #res2[0]: temperature, res2[1]: true L11, res2[2]: true L12
         #res2[3]: computed L11, res2[4]: computed L12, res2[5]: chemical potential
-        
-        for i in range(sample):
-            res2[i,0]=np.linspace(0,1, size+1)[1:]
-            res2[i,1]=labels.detach().cpu().numpy()[2*i*size:(2*i+1)*size]
-            res2[i,2]=labels.detach().cpu().numpy()[(2*i+1)*size:(2*i+2)*size]
-            res2[i,3]=preds.detach().cpu().numpy()[2*i*size:(2*i+1)*size]
-            res2[i,4]=preds.detach().cpu().numpy()[(2*i+1)*size:(2*i+2)*size]
-            res2[i,5]=chem
 
-        res.tofile('./data/exp_focus_cond_'+str(int(x*1000))+'_'+str(int(y*1000))+'_'+str(epoch+1)+'_delta_'+str(int(Delta*1000))+'.dat')
-        res2.tofile('./data/exp_focus_L11_L12_'+str(int(x*1000))+'_'+str(int(y*1000))+'_'+str(epoch+1)+'_delta_'+str(int(Delta*1000))+'.dat')
+        for i in range(sample):
+            res2[i, 0] = np.linspace(0, 1, size + 1)[1:]
+            res2[i, 1] = labels.detach().cpu().numpy()[2 * i *
+                                                       size:(2 * i + 1) * size]
+            res2[i, 2] = labels.detach().cpu().numpy()[(2 * i + 1) *
+                                                       size:(2 * i + 2) * size]
+            res2[i, 3] = preds.detach().cpu().numpy()[2 * i *
+                                                      size:(2 * i + 1) * size]
+            res2[i, 4] = preds.detach().cpu().numpy()[(2 * i + 1) *
+                                                      size:(2 * i + 2) * size]
+            res2[i, 5] = chem
+
+        res.tofile('./data/exp_focus_cond_' + str(int(x * 1000)) + '_' +
+                   str(int(y * 1000)) + '_' + str(epoch + 1) + '_delta_' +
+                   str(int(Delta * 1000)) + '.dat')
+        res2.tofile('./data/exp_focus_L11_L12_' + str(int(x * 1000)) + '_' +
+                    str(int(y * 1000)) + '_' + str(epoch + 1) + '_delta_' +
+                    str(int(Delta * 1000)) + '.dat')
 
 
 if __name__ == '__main__':
@@ -117,8 +130,10 @@ if __name__ == '__main__':
     if device.type == 'cuda':
         print(torch.cuda.get_device_name(0))
         print('Memory Usage:')
-        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024**3, 1),
+              'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024**3, 1),
+              'GB')
 
     # initialize model (and put it to device)
     model = NN.FCN(hyperparameters.fcs).to(device)
@@ -128,46 +143,57 @@ if __name__ == '__main__':
 
     # define optimizer
     optimizer = optim.Adam(model.parameters(), lr=hyperparameters.lr)
-    
+
     # loss function
-    loss_array=np.zeros(hyperparameters.epochs)
-    
+    loss_array = np.zeros(hyperparameters.epochs)
+
     #chemical potential and spectral conductivity are imported
-    epoch=5000
-    sample=7
-    data1 = np.fromfile('./data/exp_all_cond_'+str(epoch)+'_'+str(sample)+'.dat', dtype=np.float64, count=5000)
-    data2 = np.fromfile('./data/exp_all_L11_L12_'+str(epoch)+'_'+str(sample)+'.dat', dtype=np.float64, count=7*6*1000)
-    nsite=len(data1)//2
-    nsite2=len(data2)//(6*7)
+    epoch = 5000
+    sample = 7
+    data1 = np.fromfile('./data/exp_all_cond_' + str(epoch) + '_' +
+                        str(sample) + '.dat',
+                        dtype=np.float64,
+                        count=5000)
+    data2 = np.fromfile('./data/exp_all_L11_L12_' + str(epoch) + '_' +
+                        str(sample) + '.dat',
+                        dtype=np.float64,
+                        count=7 * 6 * 1000)
+    nsite = len(data1) // 2
+    nsite2 = len(data2) // (6 * 7)
     #index for a given doping concentration
-    xarray=np.array([0.0,0.0,0.001,0.002,0.01,0.02,0.05])
-    yarray=np.array([0.0,0.05,0.0,0.0,0.0,0.0,0.0])
-    ind=np.where((xarray==hyperparameters.x) & (yarray==hyperparameters.y))[0]
+    xarray = np.array([0.0, 0.0, 0.001, 0.002, 0.01, 0.02, 0.05])
+    yarray = np.array([0.0, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0])
+    ind = np.where((xarray == hyperparameters.x)
+                   & (yarray == hyperparameters.y))[0]
     #load spectral conductivity and chemical potential for a given doping concentration
-    data2=data2.reshape(7,6,nsite2)
-    data1=data1.reshape(2,nsite)
-    cond=torch.from_numpy(data1[1].astype(np.float32)).clone()
-    cond=cond.to(device)
-    alpha=torch.from_numpy(data2[ind,5].astype(np.float32)).clone()
-    alpha=alpha[0].to(device)
+    data2 = data2.reshape(7, 6, nsite2)
+    data1 = data1.reshape(2, nsite)
+    cond = torch.from_numpy(data1[1].astype(np.float32)).clone()
+    cond = cond.to(device)
+    alpha = torch.from_numpy(data2[ind, 5].astype(np.float32)).clone()
+    alpha = alpha[0].to(device)
     ## parameter for small correction in chemical potential
-    Delta=0.0
-   
-    lr=hyperparameters.lr
-    lmin=lr/5.0
+    Delta = 0.04
+
+    lr = hyperparameters.lr
+    lmin = lr / 5.0
     for epoch in range(hyperparameters.epochs):
-        if epoch>1:
-            if loss_array[epoch-2]<loss_array[epoch-1]:
-                for g in optimizer.param_groups:                   
-                    if loss_array[epoch-1]<lmin:
-                        lmin=loss_array[epoch-1]
-                    lr=max(lr*0.8,lmin)
+        if epoch > 1:
+            if loss_array[epoch - 2] < loss_array[epoch - 1]:
+                for g in optimizer.param_groups:
+                    if loss_array[epoch - 1] < lmin:
+                        lmin = loss_array[epoch - 1]
+                    lr = max(lr * 0.8, lmin)
                     g['lr'] = lr
-        if epoch%100==0:
+        if epoch % 100 == 0:
             with utils.Timer('epoch {} timing: '.format(epoch)):
-                train(device, epoch, hyperparameters.params, model, optimizer,hyperparameters.epochs,loss_array,hyperparameters.x,hyperparameters.y,alpha,cond,Delta)
-            print('--'*10)
+                train(device, epoch, hyperparameters.params, model, optimizer,
+                      hyperparameters.epochs, loss_array, hyperparameters.x,
+                      hyperparameters.y, alpha, cond, Delta)
+            print('--' * 10)
         else:
-            train(device, epoch, hyperparameters.params, model, optimizer,hyperparameters.epochs,loss_array,hyperparameters.x,hyperparameters.y,alpha,cond,Delta)
+            train(device, epoch, hyperparameters.params, model, optimizer,
+                  hyperparameters.epochs, loss_array, hyperparameters.x,
+                  hyperparameters.y, alpha, cond, Delta)
     loss_array.tofile('./data/exp_focus_loss_'+str(int(hyperparameters.x*1000))\
                 +'_'+str(int(hyperparameters.y*1000))+'_'+str(epoch+1)+'_delta_'+str(int(Delta*1000))+'.dat')
--- main_toy2.py
+++ main_toy2.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 
 # load PyTorch modules
 import torch
-from torch.utils import data # to define a dataset class
+from torch.utils import data  # to define a dataset class
 import torch.nn as nn
 #import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim import lr_scheduler
 
-# set random seed for reproducibility 
+# set random seed for reproducibility
 seed = 200
 np.random.seed(seed)
 torch.manual_seed(seed)
@@ -26,32 +26,32 @@ if torch.cuda.is_available():
     torch.backends.cudnn.benchmark = False
 
 
-def train(device, epoch, params, model, optimizer,Tot_epoch,loss_array):
+def train(device, epoch, params, model, optimizer, Tot_epoch, loss_array):
     # define data set and associated loader
     loaded_set = dataloader.Dataset()
     loader = data.DataLoader(loaded_set, **params)
-    
+
     # set model to training mode
     model.train()
-    Wc=5.0
-    sample=3
-    size=50
+    Wc = 5.0
+    sample = 3
+    size = 50
     epoch_loss = 0.0
     # dict that consists of two lists to store inputs and labels
-    results = {'inputs':[], 'preds':[], 'labels':[]}
-    
+    results = {'inputs': [], 'preds': [], 'labels': []}
+
     for inputs, labels in loader:
-        inputs = inputs[:,0].to(device)
-        labels = labels[:,0].to(device)
-        
-		# zero the parameter gradients
+        inputs = inputs[:, 0].to(device)
+        labels = labels[:, 0].to(device)
+
+        # zero the parameter gradients
         optimizer.zero_grad()
 
         # computed values of L11 and L12 from the NN
-        preds = model(inputs[:size],Wc,sample)
+        preds = model(inputs[:size], Wc, sample)
         #the penalty term of negative spectral conductivity
-        b=1.0/(2*size+1)
-        preds[-1]=preds[-1]*b
+        b = 1.0 / (2 * size + 1)
+        preds[-1] = preds[-1] * b
         #evaluate loss
         loss = criterion(preds, labels)
 
@@ -60,55 +60,61 @@ def train(device, epoch, params, model, optimizer,Tot_epoch,loss_array):
         optimizer.step()
 
         # running loss update
-        epoch_loss += loss*inputs.size(0)
+        epoch_loss += loss * inputs.size(0)
 
         with torch.no_grad():
             # push data to dict
             results['inputs'].append(inputs.detach().cpu().numpy())
             results['preds'].append(preds.detach().cpu().numpy())
             results['labels'].append(labels.detach().cpu().numpy())
-            
-    epoch_loss = epoch_loss/len(loaded_set)
-    loss_array[epoch]=epoch_loss.detach()
-    
+
+    epoch_loss = epoch_loss / len(loaded_set)
+    loss_array[epoch] = epoch_loss.detach()
+
     #generate output files after all epochs
 
-    if (epoch+1==Tot_epoch):
+    if (epoch + 1 == Tot_epoch):
         print('epoch {} Training Loss: {:.4g}'.format(epoch, epoch_loss))
         #output of NN
-        y=model.fc_layers(inputs[:size]).detach().cpu().numpy()
-        
+        y = model.fc_layers(inputs[:size]).detach().cpu().numpy()
+
         ##output the spectral conductivity
         # averaging 5 consequtive elements of the output vector to compute the spectral conductivity
-        Q=y[:size*4+10]
-        Sigma=np.zeros(size*4+1)  
+        Q = y[:size * 4 + 10]
+        Sigma = np.zeros(size * 4 + 1)
         for j in range(Sigma.shape[0]):
-            Sigma[j]=(Q[j]+Q[j+1]+Q[j+2]+Q[j+3]+Q[j+4]+Q[j+5]+Q[j+6]+Q[j+7]+Q[j+8]+Q[j+9])/10.0
-        res=np.zeros((2,size*4+1))
-        res[0]=np.linspace(-1.0, 1.0, size*4+1)*Wc*2
-        res[1]=Sigma 
-        
+            Sigma[j] = (Q[j] + Q[j + 1] + Q[j + 2] + Q[j + 3] + Q[j + 4] +
+                        Q[j + 5] + Q[j + 6] + Q[j + 7] + Q[j + 8] +
+                        Q[j + 9]) / 10.0
+        res = np.zeros((2, size * 4 + 1))
+        res[0] = np.linspace(-1.0, 1.0, size * 4 + 1) * Wc * 2
+        res[1] = Sigma
+
         ##output the chemical potential
-        chem_Q=y[size*4+10:]
-        
+        chem_Q = y[size * 4 + 10:]
+
         ##output L11 and L12 computed from the spectral conductivity
-        res2=np.zeros((sample,6,size))
+        res2 = np.zeros((sample, 6, size))
         #res2[0]: temperature, res2[1]: true L11, res2[2]: true L12
         #res2[3]: computed L11, res2[4]: computed L12, res[5]: chemical potential
-        
-        for i in range(sample):
-            res2[i,0]=np.linspace(0,1, size+1)[1:]
-            res2[i,1]=labels.detach().cpu().numpy()[2*i*size:(2*i+1)*size]
-            res2[i,2]=labels.detach().cpu().numpy()[(2*i+1)*size:(2*i+2)*size]
-            res2[i,3]=preds.detach().cpu().numpy()[2*i*size:(2*i+1)*size]
-            res2[i,4]=preds.detach().cpu().numpy()[(2*i+1)*size:(2*i+2)*size]
-            chem=chem_Q[i*3]+0*chem_Q[i*3+1]*res2[i,0]+chem_Q[i*3+2]*res2[i,0]**2
-            res2[i,5]=np.arctan(chem)/(np.pi/2.0)*Wc
-        res.tofile('./data/toy_cond_'+str(epoch+1)+'_'+str(sample)+'.dat')
-        res2.tofile('./data/toy_L11_L12_'+str(epoch+1)+'_'+str(sample)+'.dat')
-         
-    
 
+        for i in range(sample):
+            res2[i, 0] = np.linspace(0, 1, size + 1)[1:]
+            res2[i, 1] = labels.detach().cpu().numpy()[2 * i *
+                                                       size:(2 * i + 1) * size]
+            res2[i, 2] = labels.detach().cpu().numpy()[(2 * i + 1) *
+                                                       size:(2 * i + 2) * size]
+            res2[i, 3] = preds.detach().cpu().numpy()[2 * i *
+                                                      size:(2 * i + 1) * size]
+            res2[i, 4] = preds.detach().cpu().numpy()[(2 * i + 1) *
+                                                      size:(2 * i + 2) * size]
+            chem = chem_Q[i * 3] + 0 * chem_Q[i * 3 + 1] * res2[i, 0] + chem_Q[
+                i * 3 + 2] * res2[i, 0]**2
+            res2[i, 5] = np.arctan(chem) / (np.pi / 2.0) * Wc
+        res.tofile('./data/toy_cond_' + str(epoch + 1) + '_' + str(sample) +
+                   '.dat')
+        res2.tofile('./data/toy_L11_L12_' + str(epoch + 1) + '_' +
+                    str(sample) + '.dat')
 
 
 if __name__ == '__main__':
@@ -120,10 +126,11 @@ if __name__ == '__main__':
     if device.type == 'cuda':
         print(torch.cuda.get_device_name(0))
         print('Memory Usage:')
-        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024**3, 1),
+              'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024**3, 1),
+              'GB')
 
-   
     # initialize model (and put it to device)
     model = NN.FCN(hyperparameters.fcs).to(device)
 
@@ -133,26 +140,27 @@ if __name__ == '__main__':
     # define optimizer
     optimizer = optim.Adam(model.parameters(), lr=hyperparameters.lr)
     # loss function
-    loss_array=np.zeros(hyperparameters.epochs)
-    sample=3
+    loss_array = np.zeros(hyperparameters.epochs)
+    sample = 3
 
-    lr=hyperparameters.lr
-    lmin=lr/5.0
+    lr = hyperparameters.lr
+    lmin = lr / 5.0
     for epoch in range(hyperparameters.epochs):
-        if epoch>1:
-            if loss_array[epoch-2]<loss_array[epoch-1]:
-                for g in optimizer.param_groups:                   
-                    if loss_array[epoch-1]<lmin:
-                        lmin=loss_array[epoch-1]
-                    lr=max(lr*0.8,lmin)
+        if epoch > 1:
+            if loss_array[epoch - 2] < loss_array[epoch - 1]:
+                for g in optimizer.param_groups:
+                    if loss_array[epoch - 1] < lmin:
+                        lmin = loss_array[epoch - 1]
+                    lr = max(lr * 0.8, lmin)
                     g['lr'] = lr
-        if epoch%100==0:
+        if epoch % 100 == 0:
             with utils.Timer('epoch {} timing: '.format(epoch)):
-                train(device, epoch, hyperparameters.params, model, optimizer,hyperparameters.epochs,loss_array)
-            print('--'*10)
+                train(device, epoch, hyperparameters.params, model, optimizer,
+                      hyperparameters.epochs, loss_array)
+            print('--' * 10)
         else:
-            train(device, epoch, hyperparameters.params, model, optimizer,hyperparameters.epochs,loss_array)
-            
-            
-    loss_array.tofile('./data/toy_loss_'+str(epoch+1)+'_'+str(sample)+'.dat')        
+            train(device, epoch, hyperparameters.params, model, optimizer,
+                  hyperparameters.epochs, loss_array)
 
+    loss_array.tofile('./data/toy_loss_' + str(epoch + 1) + '_' + str(sample) +
+                      '.dat')
